98019b5007c401d2865dac66e8504edf7e1669c0
Joss Wang <jossome.one@gmail.com>
2017-10-12 03:32:01 -0400

    Really fixed the error this time. I'll eat shit if it is still wrong.

--- README.md
+++ README.md
@@ -6,8 +6,6 @@
 - [X] Apriori(AS94b)
     - Added dinstinguish tags for each column, since they are not the same thing even if they are of the same value. The format is a tuple: (column, value).
     - All the frequent sets are in a list, each set is a tuple.
-    - If we set the min\_sup at 80%, we can have a runtime around 5.6s.
-    - If we set the min\_sup at 60%, the runtime will rise to 18s. 
 - [ ] FP-growth(HPY00)
 - [ ] Improved Apriori of mine
 
--- apriori.py
+++ apriori.py
@@ -4,19 +4,20 @@ from itertools import combinations
 from functools import reduce
 import time
 
-def apriori(D, min_sup):
-    L = []
-    # 1-itemset
+def find_freq_1_itemset(D):
     tmp = []
     for col in D.columns:
         col_cnt = df[col].value_counts()
         col_cnt.index = [(col, x) for x in col_cnt.index]
         tmp.append(col_cnt)
-    sup_cnt = pd.concat(tmp).sort_values(ascending = False)
+    return pd.concat(tmp)
+
+def apriori(D, min_sup):
+    L = []
+    sup_cnt = find_freq_1_itemset(D)
     
-    L.append([(x,) for x in sup_cnt.index if sup_cnt[x] > min_sup])
+    L.append([(x,) for x in sup_cnt.index if sup_cnt[x] >= min_sup])
     while len(L[-1]) > 0:
-        Lk = []
         C = apriori_gen(L[-1])
         for _, t in df.iterrows():
             for c in C:
@@ -31,15 +32,15 @@ def apriori_gen(L):
     for l1 in L:
         for l2 in L:
             if all([l1[i] == l2[i] for i in range(len(l1) - 1)]) and str(l1[-1]) < str(l2[-1]):
-                c = tuple(set(l1).union(set(l2)))
+                #Tuple is hashable as index in dict, but there are sequence issues.
+                c = tuple(sorted(set(l1).union(set(l2)), key = str))
                 if has_infreq_subset(c, L): pass
                 else: C[c] = 0
     return C
     
 def has_infreq_subset(c, L):
     for s in combinations(c, len(c) - 1):  #generate each subset s of c
-        #print(s)
-        if s not in L: 
+        if s not in L:
             return True
     return False
 
@@ -53,7 +54,7 @@ if __name__ == "__main__":
             "marital-status", "occupation", "relationship", "race", "sex",\
             "capital-gain", "capital-loss", "hours-per-week", "native-country", "divide"]
 
-    L = apriori(df, min_sup = len(df) * 0.6)[:-1] #The last one is empty set, so drop it.
+    L = apriori(df, min_sup = len(df) * 0.8)
     print(reduce((lambda x, y: x + y), L))
 
     print("Runtime:", round(time.time() - start, 2), "seconds.")
