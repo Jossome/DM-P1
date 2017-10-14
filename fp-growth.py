e76e761ef0ffde37d948fcac160d7d6afb18d2d7
Joss Wang <jossome.one@gmail.com>
2017-10-14 03:43:47 -0400

    Add trace of each tree node

--- fp-growth.py
+++ fp-growth.py
@@ -5,23 +5,24 @@ from functools import reduce
 import time
 
 class tree:
-    def __init__(self, name):
+    def __init__(self, name, trace):
         self.name = name
+        self.trace = trace
         self.count = 1
         self.children = {}
         
-
 def traverse(T):
     if len(T.children) == 0:
-        print(T.name, " : ", T.count)
+        print(T.name, " : ", T.trace)
         return 
     else:
-        print(T.name, " : ", T.count)
+        print(T.name, " : ", T.trace)
         for each in T.children:
             print("Parent: ", T.name)
             traverse(T.children[each])
         print("===========")
 
+# not quite the name, because we return all the freq.
 def find_freq_1_itemset(D):
     tmp = []
     for col in D.columns:
@@ -30,14 +31,11 @@ def find_freq_1_itemset(D):
         tmp.append(col_cnt)
     return pd.concat(tmp)
 
-def fp_growth(fp_tree, D):
-    return
-
 def insert_tree(row, T):
     if len(row) == 0: return T
     
     if row[0] not in T.children:
-        T.children[row[0]] = tree(row[0])
+        T.children[row[0]] = tree(row[0], T.trace + "->" + str(T.name))
     else:
         T.children[row[0]].count += 1
         
@@ -46,13 +44,18 @@ def insert_tree(row, T):
 def tree_gen(D):
     sup_cnt = find_freq_1_itemset(D)
     sort_key = {name: i for i, name in enumerate(sup_cnt.index)} 
-    fp_tree = tree("null")
+    fp_tree = tree("null", "")
     for _, t in D.iterrows():
         row = sorted(zip(t.index, list(t)), key = lambda x: sort_key[x], reverse = True)
         insert_tree(row, fp_tree)
         
     return fp_tree
     
+def fp_growth(fp_tree, D):
+    
+    return
+
+
 
 if __name__ == "__main__":
     
